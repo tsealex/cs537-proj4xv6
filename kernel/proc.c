737c2cc1ebdbfb86cea2d9d0f8ef488179c4cbbe
tsealex <atse@wisc.edu>
2016-11-23 07:19:21 -0600

    no panic

--- kernel/proc.c
+++ kernel/proc.c
@@ -167,7 +167,7 @@ clone(void(*fcn)(void*), void *arg, void*stack)
 {
   // check whether or not stack is >= PGSIZE in size and page aligned
   if (((uint)stack & ~0xFFF) != (uint)stack || existva(proc->pgdir, stack) == 0 || ((uint)stack + PGSIZE) > USERTOP) {
-    panic("clone bad stack");
+    cprintf("clone bad stack\n");
     return -1;
   }
 
