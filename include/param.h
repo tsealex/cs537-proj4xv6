cda2f241b485da295d32222ff9d795b63e5fdd0e
tsealex <atse@wisc.edu>
2016-11-23 03:40:41 -0600

    fixed typo

--- include/param.h
+++ include/param.h
@@ -15,6 +15,6 @@
 #define USERTOP  0xA0000 // end of user address space
 #define PHYSTOP  0x1000000 // use phys mem up to here as free pool
 #define MAXARG       32  // max exec arguments
-#define PGSIZE		4096		// bytes mapped by a page
+#define PGSIZE     4096		// bytes mapped by a page
 
 #endif // _PARAM_H_
